59c136c7fd20069553753603e9e3da7207eb7684
avinash-anand <avinash.anand@digital.hmrc.gov.uk>
2016-10-17 22:09:54 +0100

    Assignment 2 submitted

---
+++ module2-solution/app.js
@@ -0,0 +1,87 @@
+( function() {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService']
+  function ToBuyController(ShoppingListCheckOffService) {
+
+    var toBuy = this;
+
+    toBuy.items = ShoppingListCheckOffService.getToBuyList();
+
+    toBuy.removeItem = function (itemIndex, itemName, quantity) {
+      ShoppingListCheckOffService.addToBoughtList(itemName, quantity);
+      ShoppingListCheckOffService.removeFromToBuyList(itemIndex);
+    };
+
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService']
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+
+    var bought = this;
+    bought.items = ShoppingListCheckOffService.getBoughtList();
+
+  }
+
+  function ShoppingListCheckOffService() {
+
+    var service = this;
+
+    var shoppingList = [
+      {
+        name: "KitKats",
+        quantity: "10"
+      },
+      {
+        name: "Donuts",
+        quantity: "5"
+      },
+      {
+        name: "Cookies",
+        quantity: "10"
+      },
+      {
+        name: "Eclairs",
+        quantity: "15"
+      },
+      {
+        name: "Sugary Drinks",
+        quantity: "10"
+      }
+    ];
+
+    var toBuyList = shoppingList;
+
+    var boughtList = [];
+
+    service.getToBuyList = function () {
+      return toBuyList;
+    };
+
+    service.removeFromToBuyList = function (itemIndex) {
+      console.log("removeFromToBuyList --> index = " + itemIndex);
+      toBuyList.splice(itemIndex, 1);
+    };
+
+    service.getBoughtList = function () {
+      return boughtList;
+    };
+
+    service.addToBoughtList = function (itemName, quantity) {
+
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      console.log("pushed --> " + item.itemName + " " + item.quantity);
+      boughtList.push(item);
+    };
+
+  }
+
+})();
